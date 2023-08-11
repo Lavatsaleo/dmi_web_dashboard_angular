0f4c6259b1fcabb301a880827606627c014d7778
Lavatsaleo <lavatsaleo@gmail.com>
2023-08-11 13:27:20 +0300

    addroutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdashboardComponent } from './adashboard/adashboard.component';
+import { SdashboardComponent } from './sdashboard/sdashboard.component';
+import { MdashboardComponent } from './mdashboard/mdashboard.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'MortalityDashboard', component:MdashboardComponent},
+  {path: 'AFIDashboard', component:AdashboardComponent},
+  {path: 'SARI/ILIDashboard',component:SdashboardComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
